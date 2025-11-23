858b3397c48c73e8bcee6676f22212e9eb6199ed
maggku <maggie.kuczynska@gmail.com>
2025-11-23 11:19:31 +0000

    Update forms.py with class RegisterForm

--- play/forms.py
+++ play/forms.py
@@ -1,1 +1,7 @@
+from django import forms
 
+class RegisterForm(forms.Form):
+  name = forms.CharField(max_length=64)
+  email = forms.EmailField()
+  country = forms.CharField(max_length=64)
+  age = forms.IntegerField()
